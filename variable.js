9fdcaf55f9dd7e8236dcea8cef8b626f5d16731b
Hasib787 <mr.hasib45@gmail.com>
2021-01-16 12:51:06 +0600

    adding some variable

--- variable.js
+++ variable.js
@@ -3,4 +3,21 @@ var bookPrice=120;
 var myPhonePrice=10000;
 
 console.log(bookPrice);
+console.log(typeof bookPrice);
 console.log(myPhonePrice);
+
+// String variable
+var fatherName="Md. Hannan Khalifa";
+var motherName="Selina Begom";
+
+console.log(fatherName);
+console.log(typeof fatherName);
+console.log(motherName);
+
+//Boolean variable.
+var isCold=true;
+var isHappyCodding=true;
+
+console.log(isCold);
+console.log(typeof isCold);
+console.log(isHappyCodding);
