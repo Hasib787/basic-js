d5b12feb27ed03eabffe06d61e75a8d76feee002
Hasib787 <mr.hasib45@gmail.com>
2021-01-17 03:04:31 +0600

    adding math file

---
+++ math.js
@@ -0,0 +1,19 @@
+// Math.abs() absoluteNumber
+var number = - 25.5555;
+var absoluteNumber = Math.abs(number); //it gives a absolute value
+console.log(absoluteNumber);
+
+// Math.round() Round function example
+var number1 = 5.99999999;
+var result = Math.round(number1); // It's give a round value of a fraction number
+console.log(result);
+
+// Math.ceil() Ceil function example
+var number2=6.045;
+var result=Math.ceil(number2); // It's give a highest value of the fraction number
+console.log(result);
+
+// Math.floor() Floor function example
+var number4=12.89965;
+var result=Math.floor(number4); // It's give a lowest value of the fraction number
+console.log(result);
