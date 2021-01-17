28405ee94bf641e82407f1d5d3a12980260c2004
Hasib787 <mr.hasib45@gmail.com>
2021-01-18 00:26:40 +0600

    adding some file

--- array.js
+++ array.js
@@ -1,8 +1,19 @@
 // Array practice
-var friendsAge=[25,14,18,27,30];
-var HasanAge=friendsAge[2];
+var friendsAge = [25, 14, 18, 27, 30];
+var HasanAge = friendsAge[2];
 
-var position=friendsAge.indexOf(18);
+var position = friendsAge.indexOf(18);
 console.log(position);
 console.log(HasanAge);
 console.log(friendsAge);
+
+friendsAge.push(40);
+friendsAge.push(22);
+console.log(friendsAge);
+friendsAge.pop();
+console.log(friendsAge);
+friendsAge.shift();
+console.log(friendsAge);
+friendsAge.unshift(787);
+console.log(friendsAge);
+
