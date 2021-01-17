81548e2b04e8d5dcc8ef322bffddee5576ca35ff
Hasib787 <mr.hasib45@gmail.com>
2021-01-17 17:37:34 +0600

    some changes

--- conditional.js
+++ conditional.js
@@ -6,3 +6,22 @@ if (phonePrice > 10) {
     console.log("I will go back home");
 
 }
+
+// Multiple Conditional Example
+
+var gotJob=false;
+var savingAmount=5000000;
+
+if (gotJob==true || savingAmount>200000) {
+    console.log("Asho tomar patri khuji");
+}
+    else if(gotJob==true){
+        console.log("Accha koydin pore dekhbo");
+
+    }
+else {
+    console.log("Tor kopale biya nai");
+}
+
+// Average and grade of the student
+
