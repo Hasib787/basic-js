377a6a2ea322ff0c6274c5cb145dec7b6cc55bf9
Hasib787 <mr.hasib45@gmail.com>
2021-01-17 22:34:16 +0600

    adding a file

---
+++ array.js
@@ -0,0 +1,8 @@
+// Array practice
+var friendsAge=[25,14,18,27,30];
+var HasanAge=friendsAge[2];
+
+var position=friendsAge.indexOf(18);
+console.log(position);
+console.log(HasanAge);
+console.log(friendsAge);
