eb39eae5de434d8b361d565446e62079c820fac7
Hasib787 <mr.hasib45@gmail.com>
2021-01-18 22:30:45 +0600

    some changes

--- array.js
+++ array.js
@@ -1,7 +1,7 @@
 // Array practice
 var friendsAge = [25, 14, 18, 27, 30];
 var HasanAge = friendsAge[2];
-friendsAge[1]=8;
+friendsAge[1] = 8;
 console.log(friendsAge);
 
 var position = friendsAge.indexOf(18);
@@ -23,10 +23,10 @@ console.log(friendsAge);
 //slice and splice
 
 var bankLine = ['Hasib', 'Moni', 'Sweety', 'Swarna', 'Surovi'];
-var part= bankLine.slice(git 1,3);
+var part = bankLine.slice(1, 3);
 console.log(part);
 console.log(bankLine);
-var anotherPart=bankLine.splice(2,3);
+var anotherPart = bankLine.splice(2, 3);
 console.log(anotherPart);
 console.log(bankLine);
 //
--- function.js
+++ function.js
@@ -10,16 +10,33 @@ function doubleIt(num) {
     var result = num * 2;
     return result;
 }
-var first=doubleIt(10);
-var second=doubleIt(50);
-var total=first+second;
+var first = doubleIt(10);
+var second = doubleIt(50);
+var total = first + second;
 console.log(total);
 
 //function with multiple parameters
 
-function addNumber(num1, num2){
-    var result=num1+num2;
+function addNumber(num1, num2) {
+    var result = num1 + num2;
     return result;
 }
-var sum = addNumber(5,20);
+var sum = addNumber(5, 20);
 console.log(sum);
+
+
+// finding largest number
+
+function largestNumber(numbers) {
+    var larger = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        var element1 = numbers[i];
+        if (element1 > larger) {
+            larger = element1;
+        }
+    }
+    return larger;
+}
+var numbers = [85, 05, 86, 56, 778, 562, 466, 213];
+var output = largestNumber(numbers);
+console.log(output);
