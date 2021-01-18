ee4c715809180e24eadcd2500cc84e9e8e49e702
Hasib787 <mr.hasib45@gmail.com>
2021-01-18 12:14:13 +0600

    adding forloop file

---
+++ forloop.js
@@ -0,0 +1,15 @@
+
+// var nums = [55, 22, 77, 88, 46, 22, 456, 21, 32];
+
+// for (var i = 0; i < nums.length; i++) {
+//     var element = nums[i];
+//     console.log(element);
+// }
+
+//for example
+var friends = ['Noman', 'Aman', 'Tarit', 'Fuhad', 'Saiful', 'Rubel'];
+
+for (var j = 0; j < friends.length; j++) {
+    var friendsName = friends[j];
+    console.log(friendsName);
+}
