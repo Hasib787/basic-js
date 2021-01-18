9ffb7b406d41b202d698964f4d99be5968a77271
Hasib787 <mr.hasib45@gmail.com>
2021-01-18 22:14:46 +0600

    adding some changes

--- array.js
+++ array.js
@@ -1,6 +1,8 @@
 // Array practice
 var friendsAge = [25, 14, 18, 27, 30];
 var HasanAge = friendsAge[2];
+friendsAge[1]=8;
+console.log(friendsAge);
 
 var position = friendsAge.indexOf(18);
 console.log(position);
@@ -21,12 +23,13 @@ console.log(friendsAge);
 //slice and splice
 
 var bankLine = ['Hasib', 'Moni', 'Sweety', 'Swarna', 'Surovi'];
-var part= bankLine.slice(1,3);
+var part= bankLine.slice(git 1,3);
 console.log(part);
 console.log(bankLine);
 var anotherPart=bankLine.splice(2,3);
 console.log(anotherPart);
 console.log(bankLine);
+//
 
 
 
