411dfa2acca7aa369cd74f2e1a2680ddd4908014
Hasib787 <mr.hasib45@gmail.com>
2021-01-23 23:54:31 +0600

    add new function

--- more.js
+++ more.js
@@ -4,3 +4,8 @@ function add(firstNum, secondNum, thirdNum) {
 }
 let result = add(10, 5, 12);
 console.log(result);
+
+function add2(numbers) {
+    let sum = numbers[0] + numbers[1] + numbers[2];
+    return sum;
+}
