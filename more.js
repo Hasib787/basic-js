4f28346361d648ced2baacdab665a6ed0fb4696a
Hasib787 <mr.hasib45@gmail.com>
2021-01-24 17:43:42 +0600

    some changes

--- more.js
+++ more.js
@@ -2,20 +2,20 @@ function add(firstNum, secondNum, thirdNum) {
     let sum = firstNum + secondNum + thirdNum;
     return sum;
 }
-let result = add(10, 5, 12);
-console.log(result);
+// let result = add(10, 5, 12);
+// console.log(result);
 
 function add2(numbers) {
     let sum = numbers[0] + numbers[1] + numbers[2];
     return sum;
 }
-function add3(number){
-    let sum=[0];
+function add3(number) {
+    let sum = 0;
     for (let i = 0; i < number.length; i++) {
-        let element = number[i];
-        sum=sum+element;
+        let sum = sum + number[i];
+
     }
     return sum;
 }
-let result=add3([5,8,6]);
+let result = add3([5, 8, 6]);
 console.log(result);
---
+++ objectVsArray.js
@@ -0,0 +1,8 @@
+let ami={
+    age: 25,
+    weight:60,
+    height:68,
+    color:'skyblue'
+}
+let myAge= ami.age;
+console.log(myAge);
