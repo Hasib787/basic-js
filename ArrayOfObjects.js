1fd15fe3ee8802a370b6236e4d6a451e00495dfb
Md. Masud Rana <mr.hasib45@gmail.com>
2021-12-04 15:54:56 +0600

    maked a array

---
+++ ArrayOfObjects.js
@@ -0,0 +1,41 @@
+let cars = [
+  {
+    name: "Ford",
+    models: "Mustang, F150, F250",
+    color: "red",
+    year: "1954",
+    price: "19000",
+  },
+  {
+    name: "Chevy",
+    models: "Camaro, Corvette",
+    color: "blue",
+    year: "1954",
+    price: "20000",
+  },
+  {
+      name: "Dodge",
+      models: "Charger, Ram",
+      color: "green",
+      year: "1995",
+      price: "30000",
+  },
+
+  {
+      name: "Ford",
+      models: "Mustang, F150, F250",
+      color: "darkBlue",
+      year: "2000",
+      price: "40000",
+  },
+  {
+      name: "Chevy",
+      models: "Camaro, Corvette",
+      color: "White",
+      year: "2021",
+      priceL: "50000",
+  },
+];
+
+console.log(cars);
+
